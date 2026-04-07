c8dba4333a78124985452f4c2189cbbfe9b2d9b9
dhanyashreemh <dhanyashree@raakacloud.com>
2026-04-07 15:11:10 +0530

    gold rate

--- gold_rate/admin.py
+++ gold_rate/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import GoldRate
+from .models import GoldRate, MakingCharges, TaxConfig
 
 admin.site.register(GoldRate)
+
+@admin.register(TaxConfig)
+class TaxConfigAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return not TaxConfig.objects.exists()
+
+
+@admin.register(MakingCharges)
+class MakingChargesAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return not MakingCharges.objects.exists()
