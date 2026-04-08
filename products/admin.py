e4143cdb3b3c2c2af2f09f6d76e901ff6e1c3ddf
dhanyashreemh <dhanyashree@raakacloud.com>
2026-04-08 16:19:55 +0530

    productadmin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Product, Variant, Option, ProductImage
 
-# Register your models here.
+
+class VariantInline(admin.TabularInline):
+    model = Variant
+    extra = 0
+
+
+class OptionInline(admin.TabularInline):
+    model = Option
+    extra = 0
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 0
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "vendor", "product_type", "status"]
+    search_fields = ["title", "vendor"]
+
+    # 🔥 EVERYTHING in one place
+    inlines = [VariantInline, OptionInline, ProductImageInline]
